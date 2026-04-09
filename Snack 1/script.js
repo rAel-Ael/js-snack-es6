2e5448fc902c752eb92511f4172b61f96cf1b5c5
rAel-Ael <alediru9267@gmail.com>
2026-04-09 17:07:15 +0200

    es 1

---
+++ Snack 1/script.js
@@ -0,0 +1,32 @@
+const bici = [
+    {
+    nome: "decathlonElops900",
+    peso: 15
+},
+{
+    nome: "bianchi",
+    peso: 9
+    },
+{
+    nome: "specializedTurbo",
+    peso: 14
+    },
+{
+    nome: "pinarelloDogmaF",
+    peso: 6
+},
+{
+    nome: "cannondaleQuick",
+    peso: 10
+}
+]
+
+
+let  Bici = pesoMinore[0];
+
+for (let i = 1; i < bici.length; i++) {
+    if (pesoBici[i] < pesoMinore); 
+    pesoMinore < Bici;
+}
+
+console.log(pesoMinore);
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Snack 1:
+const bici = [
+{
+    nome: "bicibici"
+    peso: 10
+},
+//
+
+]
+
+# snack 2:
+const Squadre = [
+{
+    nome: "nome sq"
+    punti_fatti: 0
+    falli_subiti: 0
+},
+//
+
+]
